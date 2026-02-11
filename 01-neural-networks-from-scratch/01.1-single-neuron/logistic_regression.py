25949c75cdef59443c4318cec4eec3b6d3a97b74
Eshaan0110 <eshaan.adyanthaya@gmail.com>
2026-02-12 00:55:48 +0530

    that should be it

--- 01-neural-networks-from-scratch/01.1-single-neuron/logistic_regression.py
+++ 01-neural-networks-from-scratch/01.1-single-neuron/logistic_regression.py
@@ -61,3 +61,35 @@ def compute_gradients(X, y_true, y_pred):
     dw = (1/n)* np.sum((y_pred - y_true) * X)
     db = (1/n)* np.sum(y_pred - y_true)
     return dw, db
+
+#training loop
+
+learning_rate = 0.1
+epochs = 1000
+losses = []
+
+for epoch in range(epochs):
+    y_pred = predict(X, w, b)
+
+    loss = binary_cross_entropy(y, y_pred)
+    losses.append(loss)
+
+    dw, db = compute_gradients(X, y, y_pred)
+
+    w -= learning_rate * dw
+    b -= learning_rate * db
+
+    if epoch % 100 == 0:
+        print(f"Epoch {epoch} | Loss: {loss:.4f}")
+
+    
+#visualization
+
+plt.scatter(X, y, alpha=0.5, label="Data")
+
+# Decision boundary occurs when wX + b = 0
+decision_boundary = -b / w
+plt.axvline(x=decision_boundary, color='red', label="Decision Boundary")
+
+plt.legend()
+plt.show()
