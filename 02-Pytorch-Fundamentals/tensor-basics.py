8dc4e6d200fbad39ff0e1e6bb37d120064b9a00b
Eshaan0110 <eshaan.adyanthaya@gmail.com>
2026-02-17 01:22:50 +0530

    not sure what this is need to check

--- 02-Pytorch-Fundamentals/tensor-basics.py
+++ 02-Pytorch-Fundamentals/tensor-basics.py
@@ -14,11 +14,41 @@ print(torch.__version__)
 
 
 #require gradient example
+'''requires_grad=True allows us to track the gradients of the tensor during backpropagation. 
+This is essential for training neural networks, as it enables the computation of gradients with respect to the parameters. so in simple terms, 
+it allows us to calculate how much the parameters (like weights and biases)
+ should be adjusted to minimize the loss function during training.'''
+
 
 w = torch.tensor([2.0], requires_grad=True)
 b = torch.tensor([1.0], requires_grad=True)
 x = torch.tensor([3.0])
 
-y = w * x + b
+# y = w * x + b
 
+# print("y:", y)
+# print(w.requires_grad)
+# print(x.requires_grad)
+# print(y.requires_grad)
+
+#inspect gradients
+
+print("Initial values:")
+print("w:", w)
+print("b:", b)
+print("x:", x)
+
+z = w * x
+print("\nAfter multiplication z = w * x:")
+print("z:", z)
+print("z.grad_fn:", z.grad_fn)
+
+
+y = z + b
+print("\nAfter addition y = z + b:")
 print("y:", y)
+print("y.grad_fn:", y.grad_fn)
+
+print("\nAfter backward()")
+print("Gradient of w:", w.grad)
+print("Gradient of b:", b.grad)
